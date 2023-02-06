7c637a5458e812a4ba6f7f6ce121ba18068b2b7d
dhalber11 <dhalber11@charlottesvilleschools.org>
2023-02-06 11:13:38 -0500

    Hello

--- Steppercode.py
+++ Steppercode.py
@@ -19,7 +19,7 @@ limit.direction = digitalio.Direction.INPUT
 limit.pull = digitalio.Pull.UP 
 
 coils = (
-    digitalio.DigitalInOut(board.D12),  # A1
+    digitalio.DigitalInOut(board.D12),  # A1  #setup for the stepper
     digitalio.DigitalInOut(board.D11),  # A2
     digitalio.DigitalInOut(board.D9),  # B1
     digitalio.DigitalInOut(board.D10),  # B2
@@ -29,7 +29,7 @@ coils = (
 for coil in coils:
     coil.direction = digitalio.Direction.OUTPUT
 
-smoothing = lib.smoothing.Smoothing()
+smoothing = lib.smoothing.Smoothing()  #defining the library of smoothing.py in this file
 Steps= 0 
 motor = stepper.StepperMotor(coils[0], coils[1], coils[2], coils[3], microsteps=None)
 
@@ -37,29 +37,33 @@ motor = stepper.StepperMotor(coils[0], coils[1], coils[2], coils[3], microsteps=
 while True:
     #smoothvalue = smoothing.update(int(simpleio.map_range(pot.value,0,65535,0,5500)))
     #smoothvalue = int(simpleio.map_range(pot.value,0,65535,0,4500))
-    
+     
 
-    smoothvalue = int(simpleio.map_range(smoothing.update(pot.value),0,65535,0,4500))
+    smoothvalue = int(simpleio.map_range(smoothing.update(pot.value),0,65535,0,4500))   #Use smoothing to determine where the potentiometer is within our mapped range. 
     print(smoothvalue, Steps) 
     #print(limit.value) 
     
     
-    if abs(smoothvalue - Steps) >4 and limit.value == True:
+    if abs(smoothvalue - Steps) >4 and limit.value == True:    
         #print(smoothvalue, Steps) 
         
-        if smoothvalue > Steps :
+        if smoothvalue > Steps :      #moving the stepper to the mapped and smoothed value and counting the steps
            motor.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
            Steps = Steps +1       
         time.sleep(.001)  
 
 
-        if smoothvalue < Steps: 
+        if smoothvalue < Steps:       #moving the stepper backwards when neccesary. Also going to the smoothed and mapped value
            motor.onestep(style=stepper.DOUBLE, direction= stepper.FORWARD)
            Steps = Steps -1          
         time.sleep(.001) 
     
-    if limit.value == False:
-        Steps = 4500 
+    if limit.value == False:    #Limit switch that stops the servo immediately
+        while Steps > 4300:
+           motor.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+           Steps = Steps +1       
+        time.sleep(.001)  
+
 
      
  
--- testing.py
+++ testing.py
@@ -1,70 +1,121 @@
 import time
-from adafruit_motor import stepper 
-import board
-from analogio import AnalogIn
-import digitalio
-import pwmio
+import math
 import simpleio
-import lib.smoothing
- 
-pot2 = AnalogIn(board.A1) 
-pot = AnalogIn(board.A0)
-STEPS = 200 
-limit = digitalio.DigitalInOut(board.D8) 
-limit.direction = digitalio.Direction.INPUT 
-limit.pull = digitalio.Pull.UP 
+import analogio
+import digitalio
+from adafruit_motor import stepper,servo
+import board
+import pwmio
+from analogio import AnalogIn, AnalogOut 
 
-coils = (
+
+coils =[
     digitalio.DigitalInOut(board.D12),  # A1
     digitalio.DigitalInOut(board.D11),  # A2
     digitalio.DigitalInOut(board.D9),  # B1
     digitalio.DigitalInOut(board.D10),  # B2
-)
-coils2 = (
-    digitalio.DigitalInOut(board.D7),  # A1
-    digitalio.DigitalInOut(board.D6),  # A2
-    digitalio.DigitalInOut(board.D4),  # B1
-    digitalio.DigitalInOut(board.D5),  # B2
-)
-
+    ]
 for coil in coils:
     coil.direction = digitalio.Direction.OUTPUT
-for coil in coils2: 
-    coil.direction = digitalio.Direction.OUTPUT
 
-smoothing = lib.smoothing.MovingAverage()
-Steps= 0 
+stpPot = analogio.AnalogIn(board.A0)
+
+
 motor = stepper.StepperMotor(coils[0], coils[1], coils[2], coils[3], microsteps=None)
-motor2 = stepper.StepperMotor(coils2[0], coils2[1], coils2[2], coils2[3], microsteps=None)
+
+# QOL implement servoDef example code to make defintions better to change and understand.
+rotaServ = servo.Servo(pwmio.PWMOut(board.D0, duty_cycle=2 ** 15, frequency=50))
+armLeft  = servo.Servo(pwmio.PWMOut(board.D1, duty_cycle=2 ** 15, frequency=50))
+armRight = servo.Servo(pwmio.PWMOut(board.D2, duty_cycle=2 ** 15, frequency=50))
+
+
+def valMap(xPotentiometer,xRng,yRng):
+    
+
+    if xPotentiometer < 65535/2:
+        rVal = simpleio.map_range(xPotentiometer,0,65535/2,-1,-10)
+    else:
+        rVal = simpleio.map_range(xPotentiometer,65535/2,65535,10,1)
+    
+    return rVal
+
+runningMedian = []
+timeInt = 0     
+
+# QOL get rid of globals eventually 
+#   hard change need to 
+
+def medianCalc(x):
+    runningMedian.append(round(x,1))
+    if len(runningMedian) == 20:
+        runningMedian.pop(0)
+    return median(runningMedian)
+
+def median(input):
+    sortedArray = input.copy()
+    sortedArray.sort()
+    length = round(len(sortedArray)/2)
+    return sortedArray[length]
+
+lastDirec = "n"
+
+def direcManager(interval):
+    global lastDirec
+    global timeInt
+    time = timeInt
+
+    #change condition for time to subtract thats were bugg is
+    #global because needs to change value
+    if time > abs(interval) or time == 10:
+        timeInt -= abs(math.floor(interval))
+        #better way to controll direction
+        if interval >=-9 and interval <= -1:
+            motor.onestep(direction=2)
+            lastDirec = "b"
+        elif interval <= 9 and interval >= 1:    
+            motor.onestep() 
+            lastDirec = "f"
+        else: 
+            lastDirec = "s"
+    return lastDirec        
+    
+
+prevState = 0     
+GrabClose = False      
+def Grab(buttonVal):
+    btnOn = 1
+    # BtnOn is a placeHolder for the value witch represnts the button being on 
+    # because at the time of writing logic servos and buttons not implemented
+    global prevState
+    global GrabClose
+
+    if buttonVal == btnOn and buttonVal != prevState:
+        prevState = btnOn
+        not GrabClose
+        if GrabClose == True:
+            pass
+            #code for servos to open 
+        else:
+            pass
+            #code for servos to close  
+    elif buttonVal == 0:
+        prevState = 0
+     
+    
+    
+
+
 
 while True:
-    smoothvalue = smoothing.update(int(simpleio.map_range(pot.value,0,65535,0,5000)))
-    smoothvalue2 = smoothing.update(int(simpleio.map_range(pot2.value,0,65535,0,4800)))
-    print(smoothvalue, Steps) 
-
+    timeInt +=1
+    timeContoller = medianCalc(valMap(stpPot.value,[-1,-10],[10,1]))
+    # push the most recent value to runningMedian
+    # pop the oldest from the front of runningMedian if there's more than some number of elements there
+    # compute the median of runningMedian and store it in a var for this loop
+    
+    #print(stpPot.value)
+    #print(timeInt)
+    print(f"smothedVal:{timeContoller} direc: {direcManager(timeContoller)} ")
+    #direcManager(timeContoller)
     
     
-    
-    if abs(smoothvalue - Steps) >4 and limit.value == True:
-        
-         
-        if smoothvalue > Steps :
-           motor.onestep (direction=stepper.BACKWARD, style=stepper.DOUBLE)
-           Steps = Steps +1       
-        time.sleep(.001)  
-
-
-        if smoothvalue < Steps: 
-           motor.onestep(style=stepper.DOUBLE, direction= stepper.FORWARD)
-           Steps = Steps -1          
-        time.sleep(.001) 
-    
-    if limit.value == False:
-        Steps = 4500 
-
-     
- 
-     
-#while target != steps
-    #if target is > steps
-        #move stepper up one step and add 1 to steps 
